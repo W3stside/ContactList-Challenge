26ac239f49382a3a3a5479cb833d3191859a4002
David Sato <david.sato64@gmail.com>
2017-08-15 16:17:34 -0700

    CSS webpack fix + heroku push

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -79,7 +79,7 @@ module.exports = {
             test: /\.(css|scss)$/,
             // we extract the styles into their own .css file instead of having
             // them inside the js.
-            loader: ExtractTextPlugin.extract('style', 'css?modules&localIdentName=[name]---[local]---[hash:base64:5]!sass')
+            loader: 'style!css!sass'
         },
         // allows loading in local files via require() in front end
         {
